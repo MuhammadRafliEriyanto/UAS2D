04d0ee76fefc172502894005180f8ca655a5b38f
USER <raflimhmmd621@gmail.com>
2023-07-14 19:33:15 +0700

    Commit

---
+++ src/uas.java
@@ -0,0 +1,16 @@
+public class uas {
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String id;
+
+        public void uas(String id) {
+            this.id = id;
+        }
+    }
